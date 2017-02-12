2f19508a9b26c2e7b33b758d9f95752089ce9cf7
noypi <adrian.migraso@gmail.com>
2017-02-12 21:20:04 +0800

    add propertyfile capabilities

--- propertyparams.go
+++ propertyparams.go
@@ -26,6 +26,9 @@ func ReadPropertyParams(rdr io.Reader) (o Params, err error) {
 	o = Params{}
 	rb := bufio.NewReader(rdr)
 	sep := []byte{'='}
+	var namespaces []string
+	var ns string
+
 	for {
 		bbLine, _, err := rb.ReadLine()
 		if io.EOF == err {
@@ -34,11 +37,73 @@ func ReadPropertyParams(rdr io.Reader) (o Params, err error) {
 		} else if nil != err {
 			break
 		}
+		var bIsEnvVar bool
 		bbLine = bytes.TrimSpace(bbLine)
 		if 0 == len(bbLine) {
 			continue
 		} else if '#' == bbLine[0] {
 			// commented line
+			continue
+		} else if '$' == bbLine[0] {
+			bIsEnvVar = true
+		} else if bytes.HasPrefix(bbLine, []byte(":copy")) {
+			if 0 == len(ns) {
+				// invalid, should be in a namespace
+				// ignore
+				continue
+			}
+
+			srcNs := string(bytes.TrimSpace(bytes.TrimPrefix(bbLine, []byte(":copy"))))
+			if ns == srcNs {
+				// same origin, ignore
+				continue
+			}
+
+			o.For(srcNs, func(k, v string) {
+				k = strings.TrimPrefix(k, srcNs)
+				o[ns[0:len(ns)-1]+k] = v
+			})
+
+			continue
+
+		} else if bytes.HasPrefix(bbLine, []byte(":source")) {
+			if 0 < len(namespaces) {
+				// ignore, source cannot be used inside of a namespace
+				continue
+			}
+			bbSource := bytes.TrimSpace(bytes.TrimPrefix(bbLine, []byte(":source")))
+			if 0 == len(bbSource) {
+				// invalid ignore
+				continue
+			}
+			if o2, err := ReadPropertyFile(string(bbSource)); nil != err {
+				return o, err
+			} else {
+				o.Merge(o2)
+			}
+			continue
+
+		} else if nNs := countPrefix(bbLine, '['); 0 < nNs {
+			// namespace
+			if !bytes.HasSuffix(bbLine, bytes.Repeat([]byte{']'}, nNs)) {
+				// invalid, ignore
+				continue
+			}
+
+			if len(namespaces) >= nNs {
+				// truncate, we are in new namespace
+				namespaces = namespaces[0 : nNs-1]
+			}
+
+			if len(namespaces)+1 != nNs {
+				// invalid, ignore
+				continue
+			}
+
+			currNs := bbLine[nNs:]
+			currNs = currNs[0 : len(currNs)-nNs]
+			namespaces = append(namespaces, string(currNs))
+			ns = strings.Join(namespaces, ".") + "."
 			continue
 		}
 
@@ -52,7 +117,17 @@ func ReadPropertyParams(rdr io.Reader) (o Params, err error) {
 			v = v[1 : len(v)-1]
 		}
 		k := string(bytes.TrimSpace(bbKV[0]))
-		o[strings.ToLower(k)] = v
+		v = os.Expand(v, o.Env)
+
+		if 0 < len(ns) {
+			k = ns + k
+		}
+
+		if bIsEnvVar {
+			o[k] = v
+		} else {
+			o[strings.ToLower(k)] = v
+		}
 	}
 
 	return
@@ -89,4 +164,35 @@ func (this Params) getValue(s string, defaultValue interface{}) interface{} {
 	}
 
 	return v
+}
+
+func (this Params) Merge(a Params) {
+	for k, v := range a {
+		if '$' == k[0] {
+			continue
+		}
+		this[k] = os.Expand(v, this.Env)
+	}
+}
+
+func (this Params) For(prefix string, cb func(k, v string)) {
+	for k, v := range this {
+		if strings.HasPrefix(k, prefix) {
+			cb(k, v)
+		}
+	}
+}
+
+func (this Params) Env(k string) string {
+	s, _ := this["$"+k]
+	return s
+}
+
+func countPrefix(s []byte, c byte) int {
+	for i, c0 := range s {
+		if c != c0 {
+			return i
+		}
+	}
+	return 0
 }
--- propertyparams_test.go
+++ propertyparams_test.go
@@ -37,3 +37,154 @@ somek5 = ""  got some space5 ""`
 	assert.Equal("\"  got some space5 \"", o.Get("somek5", ""))
 
 }
+
+func TestParamsEnvVars(t *testing.T) {
+	assert := assertpkg.New(t)
+
+	const config = `
+$Var0 = var value
+somek0 = $Var0
+somek1 = $Var1
+$Var1 = var value2
+somek2 = $Var1
+somek3 =  some $Var0
+$Var3 = $Var0 concat
+somek4 =  $Var3
+somek5 =  ${Var3}concat`
+
+	buf := bytes.NewBufferString(config)
+	o, err := util.ReadPropertyParams(buf)
+	assert.Nil(err)
+	assert.Equal("var value", o.Get("somek0", ""))
+	assert.Equal("", o.Get("somek1", "aaaa"))
+	assert.Equal("var value2", o.Get("somek2", ""))
+	assert.Equal("some var value", o.Get("somek3", ""))
+	assert.Equal("var value concat", o.Get("somek4", ""))
+	assert.Equal("var value concatconcat", o.Get("somek5", ""))
+}
+
+func TestParamsNamespace(t *testing.T) {
+	assert := assertpkg.New(t)
+
+	const config = `
+$Var0 = var value
+somek0 = $Var0
+somek1 = $Var1
+$Var1 = var value2
+$Var3 = $Var0 concat
+
+[ns1]
+somek2 = $Var1
+somek3 =  some $Var0
+
+[[ns2]]
+somek4 =  $Var3
+somek5 =  ${Var3}concat
+
+[ns1b]
+somek2 = $Var1 b
+somek3 =  some $Var0 b
+
+[[ns2b]]
+somek4 =  $Var3 b
+somek5 =  ${Var3}concat b
+
+[[ns2c]]
+somek5 =  ${Var3}concat c
+
+[[[ns3c]]]
+somek5 =  ${Var3}concat c
+
+[[ns2d]]
+somek5 =  ${Var3}concat d
+
+[ns1e]
+somek5 =  ${Var3}concat e`
+
+	buf := bytes.NewBufferString(config)
+	o, err := util.ReadPropertyParams(buf)
+	assert.Nil(err)
+	assert.Equal("var value", o.Get("somek0", ""))
+	assert.Equal("", o.Get("somek1", "aaaa"))
+	assert.Equal("<notfound>", o.Get("somek2", "<notfound>"))
+
+	assert.Equal("var value2", o.Get("ns1.somek2", ""))
+	assert.Equal("some var value", o.Get("ns1.somek3", ""))
+	assert.Equal("var value concat", o.Get("ns1.ns2.somek4", ""), "k=%s: m=%v", "ns1.ns2.somek4", o)
+	assert.Equal("var value concatconcat", o.Get("ns1.ns2.somek5", ""))
+
+	assert.Equal("var value2 b", o.Get("ns1b.somek2", ""), "k=%s m=%v", "ns1b.somek2", o)
+	assert.Equal("some var value b", o.Get("ns1b.somek3", ""))
+	assert.Equal("var value concat b", o.Get("ns1b.ns2b.somek4", ""))
+	assert.Equal("var value concatconcat b", o.Get("ns1b.ns2b.somek5", ""))
+	assert.Equal("var value concatconcat c", o.Get("ns1b.ns2c.somek5", ""))
+	assert.Equal("var value concatconcat c", o.Get("ns1b.ns2c.ns3c.somek5", ""))
+	assert.Equal("var value concatconcat d", o.Get("ns1b.ns2d.somek5", ""))
+	assert.Equal("var value concatconcat e", o.Get("ns1e.somek5", ""))
+}
+
+func TestParamsNamespaceCopy(t *testing.T) {
+	assert := assertpkg.New(t)
+
+	const config = `
+$Var0 = var value
+somek0 = $Var0
+somek1 = $Var1
+$Var1 = var value2
+$Var3 = $Var0 concat
+
+[ns1]
+somek2 = $Var1
+somek3 =  some $Var0
+
+[[ns2]]
+somek4 =  $Var3
+somek5 =  ${Var3}concat
+
+[ns1b]
+somek2 = $Var1 b
+somek3 =  some $Var0 b
+
+[[ns2b]]
+somek4 =  $Var3 b
+somek5 =  ${Var3}concat b
+
+[[ns2c]]
+somek5 =  ${Var3}concat c
+
+[[[ns3c]]]
+:copy ns1.ns2
+
+[[[ns3d]]]
+:copy ns1.ns2
+somek5 = overwritten
+
+`
+
+	buf := bytes.NewBufferString(config)
+	o, err := util.ReadPropertyParams(buf)
+	assert.Nil(err)
+	assert.Equal("var value", o.Get("somek0", ""))
+	assert.Equal("", o.Get("somek1", "aaaa"))
+	assert.Equal("<notfound>", o.Get("somek2", "<notfound>"))
+
+	assert.Equal("var value2", o.Get("ns1.somek2", ""))
+	assert.Equal("some var value", o.Get("ns1.somek3", ""))
+	assert.Equal("var value concat", o.Get("ns1.ns2.somek4", ""), "k=%s: m=%v", "ns1.ns2.somek4", o)
+	assert.Equal("var value concatconcat", o.Get("ns1.ns2.somek5", ""))
+
+	assert.Equal("var value2 b", o.Get("ns1b.somek2", ""), "k=%s m=%v", "ns1b.somek2", o)
+	assert.Equal("some var value b", o.Get("ns1b.somek3", ""))
+	assert.Equal("var value concat b", o.Get("ns1b.ns2b.somek4", ""))
+	assert.Equal("var value concatconcat b", o.Get("ns1b.ns2b.somek5", ""))
+	assert.Equal("var value concatconcat c", o.Get("ns1b.ns2c.somek5", ""))
+
+	// copied
+	assert.Equal("var value concat", o.Get("ns1b.ns2c.ns3c.somek4", ""), "o=%v", o)
+	assert.Equal("var value concatconcat", o.Get("ns1b.ns2c.ns3c.somek5", ""))
+
+	// copied
+	assert.Equal("var value concat", o.Get("ns1b.ns2c.ns3d.somek4", ""), "o=%v", o)
+	assert.Equal("overwritten", o.Get("ns1b.ns2c.ns3d.somek5", ""))
+
+}
